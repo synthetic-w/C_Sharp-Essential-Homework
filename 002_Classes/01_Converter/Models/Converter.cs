1ccb8deb76222f7087a7f300e3e09a1f410a90b9
ChillingBore563 <teddybeargh@gmail.com>
2022-05-31 00:04:24 +0300

    Added task 01_Converter in homework for Lesson 002_Classes

---
+++ 002_Classes/01_Converter/Models/Converter.cs
@@ -0,0 +1,46 @@
+﻿namespace _01_Converter.Models
+{
+    internal class Converter
+    {
+        private readonly double usd;
+        private readonly double eur;
+        private readonly double rub;
+
+        public Converter(double usd, double eur, double rub)
+        {
+            this.usd = usd;
+            this.eur = eur;
+            this.rub = rub;
+        }
+
+        public double ConvertToUsd(double value)
+        {
+            return value / usd;
+        }
+
+        public double ConvertToEur(double value)
+        {
+            return value / eur;
+        }
+
+        public double ConvertToRub(double value)
+        {
+            return value / rub;
+        }
+
+        public double ConvertFromUsd(double value)
+        {
+            return usd * value;
+        }
+
+        public double ConvertFromEur(double value)
+        {
+            return eur * value;
+        }
+
+        public double ConvertFromRub(double value)
+        {
+            return rub * value;
+        }
+    }
+}
---
+++ 002_Classes/01_Converter/Program.cs
@@ -0,0 +1,92 @@
+﻿using _01_Converter.Models;
+
+// See https://aka.ms/new-console-template for more information
+
+/*
+ * Используя Visual Studio, создайте проект по шаблону Console Application.
+Требуется:
+Создать класс Converter.
+В теле класса создать пользовательский конструктор, который принимает три вещественных аргумента,
+и инициализирует поля соответствующие курсу 3-х основных валют, по отношению к гривне – public
+Converter(double usd, double eur, double rub).
+
+Написать программу, которая будет выполнять конвертацию из гривны в одну из указанных валют,
+также программа должна производить конвертацию из указанных валют в гривну.
+ */
+Converter converter = new Converter(29.53, 31.83, 0.46);
+
+Console.WriteLine("Нажмите:");
+Console.WriteLine("1: Для перевода в гривны");
+Console.WriteLine("2: Для перевода из гривен");
+
+switch (int.Parse(Console.ReadLine()))
+{
+    case 1:
+        ConvertTo(converter);
+        break;
+
+    case 2:
+        ConvertFrom(converter);
+        break;
+}
+
+Console.WriteLine("Выполнено");
+Console.ReadKey();
+
+void ConvertTo(Converter currencyConverter)
+{
+    Console.WriteLine("Нвжмите:");
+    Console.WriteLine("1: Для перевода из USD");
+    Console.WriteLine("2: Для перевода из EUR");
+    Console.WriteLine("3: Для перевода из RUB");
+
+    var option = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Введите сумму");
+
+    var input = double.Parse(Console.ReadLine());
+
+    switch (option)
+    {
+        case 1:
+            Console.WriteLine(currencyConverter.ConvertFromUsd(input));
+            break;
+
+        case 2:
+            Console.WriteLine(currencyConverter.ConvertFromEur(input));
+            break;
+
+        case 3:
+            Console.WriteLine(currencyConverter.ConvertFromRub(input));
+            break;
+    }
+}
+
+void ConvertFrom(Converter currencyConverter)
+{
+    Console.WriteLine("Нажмите:");
+    Console.WriteLine("1: Для перевода в USD");
+    Console.WriteLine("2: Для перевода в EUR");
+    Console.WriteLine("3: Для перевода в RUB");
+
+    var option = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Введите сумму");
+
+    var input = double.Parse(Console.ReadLine());
+
+    switch (option)
+    {
+        case 1:
+            Console.WriteLine(currencyConverter.ConvertToUsd(input));
+            break;
+
+        case 2:
+            Console.WriteLine(currencyConverter.ConvertToEur(input));
+            break;
+
+        case 3:
+            Console.WriteLine(currencyConverter.ConvertToRub(input));
+            break;
+    }
+}
