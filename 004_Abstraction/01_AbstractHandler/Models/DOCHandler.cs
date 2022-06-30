e8d041717d340df9f475706874720bbabb992291
ChillingBore563 <teddybeargh@gmail.com>
2022-06-30 22:17:16 +0300

    Added task 01_AbstractHandler for homework lesson 004_Abstraction

---
+++ 004_Abstraction/01_AbstractHandler/Models/DOCHandler.cs
@@ -0,0 +1,25 @@
+﻿namespace _01_AbstractHandler.Models
+{
+    internal class DOCHandler : AbstractHandler
+    {
+        public override void Open()
+        {
+            Console.WriteLine("Открыт файл .doc");
+        }
+
+        public override void Create()
+        {
+            Console.WriteLine("Создан файл .doc");
+        }
+
+        public override void Change()
+        {
+            Console.WriteLine("Изменён файл .doc");
+        }
+
+        public override void Save()
+        {
+            Console.WriteLine("Сохранён файл .doc");
+        }
+    }
+}
