453941e8d1bfe7e9113a6c4ca4255a0bcb4bd508
ChillingBore563 <teddybeargh@gmail.com>
2022-07-13 19:17:15 +0300

    Null

---
+++ 004_Abstraction/01_AbstractHandler/Models/TXTHandler.cs
@@ -0,0 +1,25 @@
+﻿namespace _01_AbstractHandler.Models
+{
+    internal class TXTHandler : AbstractHandler
+    {
+        public override void Open()
+        {
+            Console.WriteLine("Открыт файл .txt");
+        }
+
+        public override void Create()
+        {
+            Console.WriteLine("Создан файл .txt");
+        }
+
+        public override void Change()
+        {
+            Console.WriteLine("Изменён файл .txt");
+        }
+
+        public override void Save()
+        {
+            Console.WriteLine("Сохранён файл .txt");
+        }
+    }
+}
