a4b64b218698c1e0bc382f99172019bc7e7c62d4
ChillingBore563 <teddybeargh@gmail.com>
2022-09-13 19:42:12 +0300

    Added missing Lesson 004_Abstraction folder

---
+++ 004_Abstraction/01_AbstractHandler/Models/AbstractHandler.cs
@@ -0,0 +1,23 @@
+﻿/*
+Создайте класс AbstractHandler.
+В теле класса создать методы void Open(), void Create(), void Change(), void Save().
+Создать производные классы XMLHandler, TXTHandler, DOCHandler от базового класса
+AbstractHandler.
+Написать программу, которая будет выполнять определение документа и для каждого формата
+должны быть методы открытия, создания, редактирования, сохранения определенного
+формата документа.
+*/
+
+namespace _01_AbstractHandler.Models
+{
+    internal abstract class AbstractHandler
+    {
+        public abstract void Open();
+
+        public abstract void Create();
+
+        public abstract void Change();
+
+        public abstract void Save();
+    }
+}
